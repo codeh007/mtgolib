e3901dcd1b570997e6768d4bb825729fc6dc9b3c
a <a@a.com>
2024-01-17 00:32:42 +0000

    1

--- reverse/init.go
+++ reverse/init.go
@@ -3,7 +3,7 @@ package reverse
 import "strconv"
 
 // Int returns the decimal reversal of the integer i.
-func Int(i int) int {
+func IntDemo(i int) int {
 	i, _ = strconv.Atoi(String(strconv.Itoa(i)))
 	return i
 }
